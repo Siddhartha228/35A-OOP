5cf438136042e34fd276a7c80bb671cb9e37ef3c
Siddhartha Dhakal <dhakalsiddhartha228@gmail.com>
2024-04-11 10:52:50 +0545

    Week 1 task 3

---
+++ Week1/task3.java
@@ -0,0 +1,24 @@
+package Tasks.Week1;
+
+public class task3 {
+    public static void main(String[] args) {
+        /* 3. Write a program that prints the following output:
+
+A "quoted" String is
+
+'much' better if you learn
+
+the rules of "escape sequences."
+
+Also, "" represents an empty
+
+String. Don't forget: use \"
+
+instead of " !
+
+'' is not the same as " */
+        System.out.print("A \"quoted\" String is \n\'much\' better if you learn\nthe rules of \"escape sequences.\n");
+        System.out.print("Also,\"\" represents an empty\nString. Don't forget: use\\\"\ninstead of \" !\n ");
+        System.out.print("\'\' is not the same as \"");
+    }
+}

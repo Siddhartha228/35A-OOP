0559e2d7a112248b81399921e23ea8ae521c8aea
Siddhartha Dhakal <dhakalsiddhartha228@gmail.com>
2024-04-12 21:29:27 +0545

    week 1 task 4

---
+++ Week1/task4.java
@@ -0,0 +1,12 @@
+package Tasks.Week1;
+
+public class task4 {
+    public static void main(String[] args) {
+        /* 4. WAP that prints the following pattern:
+*
+**
+***
+**** */
+        System.out.print("*\n**\n***\n****");
+    }
+}

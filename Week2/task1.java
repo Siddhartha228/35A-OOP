d15a85052a7cb4d568b0e4d9069fdd9ad0bf6694
Siddhartha Dhakal <dhakalsiddhartha228@gmail.com>
2024-05-01 21:51:05 +0545

    week2 task1

---
+++ Week2/task1.java
@@ -0,0 +1,19 @@
+package Tasks.Week2;
+
+import java.util.Scanner;
+
+public class task1 {
+    public static void main(String[] args) {
+        /* 1. Write a program to check whether a person can cast a vote or not. The condition is you must be over 18 years old to vote. */
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age:");
+        int age = sc.nextInt();
+        sc.close();
+        if(age>18){
+            System.out.println("Eligible to vote.");
+        }
+        else{
+            System.out.println("Not eligible to vote.");
+        }
+    }
+}
